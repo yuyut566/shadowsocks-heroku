effd5e906bf1c41c531be40238c4919ac5096c8f
yuyut566 <70671466+yuyut566@users.noreply.github.com>
2020-12-15 09:30:52 +0000

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@
     res.writeHead(200, {
       'Content-Type': 'text/plain'
     });
-    return res.end("Welcome to Heroku https://github.com/onplus/shadowsocks-heroku");
+    return res.end("Welcome to Heroku https://github.com/yuyut566/shadowsocks-heroku");
   });
 
   wss = new WebSocketServer({
